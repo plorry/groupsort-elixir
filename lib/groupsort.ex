32346886356fdd02fdec6d0fd5b99d82c5fc335f
plorry <timebot@gmail.com>
2017-12-25 02:24:37 -0500

    added docs/doctest for sort method

--- lib/groupsort.ex
+++ lib/groupsort.ex
@@ -93,8 +93,28 @@ defmodule Groupsort do
   defp min_pairing_groupset(history, groupset1, groupset2),
     do: Enum.min_by([groupset1, groupset2], &(get_groupset_pair_count(history, &1)))
 
+  @doc """
+  Groupsort.sort is the primary method of this module. Given a history of student pairings,
+  a student_list (list of IDs), and a desired group_config, this function will return a
+  groupset (ie. a list of groups) matching the group_config, with the minimum number of
+  repeated pairs from the given history.
+
+  ## Examples
+    iex> history = %{{1, 2} => 4, {1, 3} => 3, {1, 4} => 0, {2, 3} => 0, {2, 4} => 2, {3, 4} => 1}
+    iex> student_list = [1, 2, 3, 4]
+    iex> Groupsort.sort(history, student_list, [2, 2])
+    [[1, 4], [2, 3]]
+
+  In the above example, we have 4 students, and we want to create a groupset that consists
+  of two groups of two (the sum of our group_config values must naturally total the count
+  of students).
+  From our history, we can see that students 1 and 4 have never been paired together. Likewise,
+  students 2 and 3 have never been paired. Every other possible pair has some non-zero count.
+  Therefore, our ideal final pairing is 1 and 4, and 2 and 3. Two groups of two, minimizing the
+  pairing overlap.
+  """
   def sort(history, student_list, group_config, groupset \\ [])
-  def sort(history, student_list, [_|[]], groupset), do: [student_list | groupset]
+  def sort(_, student_list, [_|[]], groupset), do: [student_list | groupset]
 
   def sort(history, student_list, [group_size|group_config], groupset) do
     (for group <- combinations(student_list, group_size),
